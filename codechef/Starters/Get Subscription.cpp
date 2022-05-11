41ae3535e0615a0fc3941cb80888a7761284f0ab
Prasad Acharya <50115378+prasad145@users.noreply.github.com>
2022-05-11 23:55:13 +0530

    Create Get Subscription.cpp

---
+++ codechef/Starters/Get Subscription.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        if(n > 30)
+        {
+            cout << "YES" << "\n";
+        }
+        else
+        {
+            cout << "NO" << "\n";
+        }
+    }
+    return 0;
+}
